b72fec8adf3df448c66e270eb34cfac613c913e4
Harsh-66Anandan <ha6633751@gmail.com>
2026-01-17 05:52:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.graph_objects as go
+
+set.set_page_config(
+    page_title = "Streamlit dash board",
+    layout= "wide"
+)
+
+
+st.title("Streamlit dash board")
+st.write("Itraday dashboard")
 
